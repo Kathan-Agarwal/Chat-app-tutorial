b18fa76ecd41e2b9e475181c66e7969e3807571a
Kathan-Agarwal <kathanhagarwal@gmail.com>
2025-05-02 21:23:18 +0530

    Added PHP to Hnadle Login with Session

--- index.php
+++ index.php
@@ -1,17 +1,17 @@
 <!DOCTYPE html>
-<html>
-<head>
-    <title> Simple Chat Application </title>
-    <link rel="stylesheet" href="style.css" />
-</head>
-<body>
-    <div id="loginform">
-        <p> Please enter your name to continue!</p>
-        <form action="index.php" method="post">
-            <label for="name">Name -</label>
-            <input type="text" name="name" id="name" />
-            <input type="submit" name="enter" value="Enter" />
-</form>
-</div>
-</body>
-</html> 
+ <html>
+ <head>
+     <title> Simple Chat Application </title>
+     <link rel="stylesheet" href="style.css" />
+ </head>
+ <body>
+     <div id="loginform">
+         <p> Please enter your name to continue!</p>
+         <form action="index.php" method="post">
+             <label for="name">Name -</label>
+             <input type="text" name="name" id="name" />
+             <input type="submit" name="enter" value="Enter" />
+ </form>
+ </div>
+ </body>
+ </html> 
